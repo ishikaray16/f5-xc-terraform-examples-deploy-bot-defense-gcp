df93829a4c84ff6851054ace87c7f44af40b69dc
Ishika RAY <i.ray@f5.com>
2025-02-04 21:16:20 +0530

    updated bot_profile.tf

--- gcp/bigip/bot-defense/bot_profile.tf
+++ gcp/bigip/bot-defense/bot_profile.tf
@@ -11,7 +11,7 @@ resource "bigip_ltm_monitor" "monitor" {
 }
 
 resource "bigip_ltm_node" "node" {
-  name                    = "/Common/terraform_node"
+  name                    = "/Common/terraform_node1"
   address                 = local.app_ip
   connection_limit        = "0"
   dynamic_ratio           = "1"
