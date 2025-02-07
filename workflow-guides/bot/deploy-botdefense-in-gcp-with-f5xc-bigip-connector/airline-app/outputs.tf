51702b810cb45a16a77a40e49a022b6d8e8729c5
Ishika RAY <i.ray@f5.com>
2025-02-07 15:50:56 +0530

    update

--- workflow-guides/bot/deploy-botdefense-in-gcp-with-f5xc-bigip-connector/airline-app/outputs.tf
+++ workflow-guides/bot/deploy-botdefense-in-gcp-with-f5xc-bigip-connector/airline-app/outputs.tf
@@ -1,4 +1,9 @@
-output "app_ip" {
+#output "app_ip" {
   #value     = local.lb_ip
-  value      = nonsensitive(kubectl_manifest.app-service)
+#  value      = nonsensitive(kubectl_manifest.app-service)
+#  sensitive  = true
+#}
+
+output "test" {
+  value      = kubectl_manifest.app-service
 }
