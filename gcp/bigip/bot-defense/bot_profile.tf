8b0368833e5288e4e38fb99178b065e1689aacea
Ishika RAY <i.ray@f5.com>
2025-02-13 20:04:50 +0530

    updated node2

--- gcp/bigip/bot-defense/bot_profile.tf
+++ gcp/bigip/bot-defense/bot_profile.tf
@@ -53,13 +53,9 @@ resource "bigip_ltm_monitor" "monitor2" {
 
 resource "bigip_ltm_node" "node2" {
   name                    = "/Common/terraform_node_bd"
-  #address                 = "/Common/ibd-webus.fastcache.net"
+  address                 = "ibd-webus.fastcache.net"
   monitor                 = "none"
   description             = "Terraform-Node-Bot-Defense"
-  fqdn {
-    address_family = "/Common/ibd-webus.fastcache.net"
-    interval       = "3000"
-  }
 }
 
 resource "bigip_ltm_pool" "pool2" {
