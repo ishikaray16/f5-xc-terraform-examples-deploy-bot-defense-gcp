196922065bd7fe7a45b3408ad964423169ae3b48
Ishika RAY <i.ray@f5.com>
2025-02-18 11:45:48 +0530

    update

--- tools/run-curl-traffic.sh
+++ tools/run-curl-traffic.sh
@@ -1,16 +1,8 @@
-# for run in {1..10}
-# do
-#   echo "Running commands against website Address - $1"
-#   curl -s "$1" -i -X POST -d "username=1&password=1"
-#   sleep 10
-#   echo CURL Credential Stuffing attempt "$run" done
-#   sleep 2
-# done
-
 for run in {1..10}
 do
-  curl -s "http://34.169.188.2/user/signin" -i -X POST -d "username=1&password=1"
-  echo
+  echo "Running commands against website Address - $1"
+  curl -s "$1" -i -X POST -d "username=1&password=1"
+  sleep 10
   echo CURL Credential Stuffing attempt "$run" done
   sleep 2
 done
