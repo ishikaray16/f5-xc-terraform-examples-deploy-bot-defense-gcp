c1efdeb8a2894eb82ac831b727a2725577121602
Ishika RAY <i.ray@f5.com>
2025-02-18 13:03:17 +0530

    update

--- tools/run-curl-traffic.sh
+++ tools/run-curl-traffic.sh
@@ -1,9 +1,10 @@
 for run in {1..10}
 do
   echo "Running commands against website Address - $1"
-  #curl -s "$1" -i -X POST -d "username=1&password=1"
-  exec curl -s "$1" -i -X POST -d "username=1&password=1"
-  sleep 30
+  COMMAND="curl -s '$1' -i -X POST -d 'username=1&password=1'"
+  echo $COMMAND
+  eval $COMMAND
+  sleep 10
   echo CURL Credential Stuffing attempt "$run" done
   sleep 2
 done
