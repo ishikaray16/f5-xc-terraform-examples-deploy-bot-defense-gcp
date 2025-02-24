b3fd2bd3aee8b97afea06344e48309e704ffc2f0
Shubham Mishra <90624610+shubhammishra9@users.noreply.github.com>
2025-02-24 15:54:14 +0530

    Update versions.tf

--- workflow-guides/bot/deploy-botdefense-in-gcp-with-f5xc-bigip-connector/airline-app/versions.tf
+++ workflow-guides/bot/deploy-botdefense-in-gcp-with-f5xc-bigip-connector/airline-app/versions.tf
@@ -6,15 +6,9 @@ terraform {
       version = "5.9.0"
     }
 
-#	kubectl = {
-#      source  = "gavinbunney/kubectl"
-#      version = ">= 1.7.0"
-#    }
-
-    kubectl = {
-      source = "alekc/kubectl"
-      version = ">=2.1.3"
+	kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.7.0"
     }
-
   }
 }
