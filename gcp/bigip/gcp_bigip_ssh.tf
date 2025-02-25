7ca4b2eaab85f02cc4f3a661d7cbec4fce8f941a
Ishika RAY <i.ray@f5.com>
2025-02-25 12:42:50 +0530

    updated deploy

--- gcp/bigip/gcp_bigip_ssh.tf
+++ gcp/bigip/gcp_bigip_ssh.tf
@@ -1,4 +1,4 @@
-resource "github_user_ssh_key" "ssh_key" {
-  title = "Creating ssh_key file"
-  key   = file(var.ssh_key)
-}
+#resource "github_user_ssh_key" "ssh_key" {
+#  title = "Creating ssh_key file"
+#  key   = file(var.ssh_key)
+#}
